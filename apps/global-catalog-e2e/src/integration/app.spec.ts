7b9162f86d7655c8d7a7d749a86ef299084a74ff
Guanghui Wang <guanghui-wang@foxmail.com>
2022-05-16 16:28:24 +0800

    e2e: fix test

--- apps/global-catalog-e2e/src/integration/app.spec.ts
+++ apps/global-catalog-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('global-catalog', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome global-catalog');
+    getGreeting().contains('Welcome to global-catalog!');
   });
 });
